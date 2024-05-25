0df86e37b25c26fbfb36da2093883d492372149e
blankuet <blancafeijoocruz@gmail.com>
2024-05-25 10:56:30 +0200

    lab done except for movie details route iteration

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const Movie = require('../models/Movie.model');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get("/movies", (req, res) => {
+    Movie.find().then((data) => {
+        res.render("movies", {movies: data});
+    });
+});
+
+router.get('movies/:id', (req, res) => {
+    Movie.findById(req.params.id).then((data) => {
+        res.render("movie", {movie: data});
+    })
+})
 
 module.exports = router;
